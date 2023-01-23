f2a47cee96cba47caa9d595e84329b9cf9481ded
Yuriy Semyenkov <etoosamoe@yandex.ru>
2023-01-23 18:10:42 +0100

    Add check open port script

---
+++ README.md
@@ -0,0 +1,6 @@
+# etosamoe's Raycast scripts
+
+My scripts for [Raycast](https://www.raycast.com/).
+
+# Scripts
+- `check-open-ports` - Checks if network port on remote host opened or closed. Uses https://www.yougetsignal.com/tools/open-ports/ for checking.
---
+++ check-open-port.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Check Open Port
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 💡
+# @raycast.argument1 { "type": "text", "placeholder": "IP Address" }
+# @raycast.argument2 { "type": "text", "placeholder": "Port" }
+# @raycast.packageName Network Tools
+
+# Documentation:
+# @raycast.description Checks if the port on remote host opened or closed.
+# @raycast.author etoosamoe
+# @raycast.authorURL https://github.com/etoosamoe
+
+curl -s -X POST https://ports.yougetsignal.com/check-port.php\?remoteAddress\=$1\&portNumber\=$2 | grep -o "Open\|Closed"
